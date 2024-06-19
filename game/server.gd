6f2a6ca01bb16651cf0e0500f3ca04d06f6d8ad8
PL3B3 <kevinqiesq@gmail.com>
2024-06-19 13:02:46 -0500

    Break up server game logic into methods
    We are currently running everything in physics process:
    - Validate all inputs, data, components exist
    - Parse the above into a map of "resources" per client id
    - Calculate the next physics state
    - Perform character abilities
    - Apply bullet damage
    - Arrange the data we need to export per client
    - Display characters (first and third person)
    - Display bullet tracers
    - Send snapshots back to clients
    
    Pretty much all the logic lives directly in the function body, which
    although convenient has the downside of:
    - visual clutter
    - less clear exactly what each sub-system depends on, in terms of
      data, objects it must access, etc.
    
    Let's break this logic up into logical parts + extract a static method
    for each part so it's clear exactly what the inputs and outputs are.
    
    Also, free-rider to this commit is we'll be changing how we format long
    method signatures. We used to do this:
    ```
    func bigMethod(
      input 1,
      input 2,
      ...
      ) -> return type:
      code 1
      code 2
    ```
    Then we did this to have the code on different indent vs. the signature
    ```
    func bigMethod(
        input 1,
        input 2,
        ...) -> return type:
      code 1
      code 2
    ```
    On second thought, it does make sense to have the return type be on its
    own line, because it's a piece of meaningful info. This is distinct
    from method calls, where the closing parens adds no info and therefore
    makes sense to just inline it
    ```
    func bigMethod(
      input 1,
      input 2,
      ...
    ) -> return type:
      code 1
      code 2
    ```

--- game/server.gd
+++ game/server.gd
@@ -49,59 +49,20 @@ func _on_client_connected(id: int) -> void:
 	resize_window(prior_peer_count)
 
 func _physics_process(_delta: float) -> void:
-	var character_resource_per_client_id: Dictionary = {}
-	for client_id: int in client_resources_per_peer_id_:
-		var client_input_buffer_and_character: InputBufferAndCharacterEntity = client_resources_per_peer_id_[client_id]
-		var latest_client_input_with_tick: QueueItem = client_input_buffer_and_character.input_buffer.pop()
-		var latest_client_input: ClientInput = latest_client_input_with_tick.value()
-		var client_tick_for_input: int = latest_client_input_with_tick.tick()
-		var character_entity_id: int = client_input_buffer_and_character.character_entity.entity_id
-		var character_state: ServerCharacterState = Utils.get_or_default(
-			world_state_, character_entity_id, client_input_buffer_and_character.character_entity.state_data)
-		character_resource_per_client_id[client_id] = ServerCharacterResource.new(
-			latest_client_input,
-			client_tick_for_input,
-			character_entity_id,
-			character_state,
-			client_input_buffer_and_character.character_entity.components)
-	
-	var next_world_state := {}
-	var data_to_export_per_client := {}
+	var tracer_displayer := entity_spawner_.get_or_create_tracer_displayer()
+	var character_resource_per_client_id := __prepare_character_resource_per_client_id(
+		client_resources_per_peer_id_, world_state_)
 	var hitscan_results := __perform_character_abilities(world_state_, character_resource_per_client_id)
-	for client_id: int in character_resource_per_client_id:
-		var character_resource: ServerCharacterResource = character_resource_per_client_id[client_id]
-		var character_entity_id := character_resource.character_entity_id
-		var character_components := character_resource.character_components
-		var latest_input_state := character_resource.input.input_state()
-		var next_physics_state := character_components.movement_body().compute_next_physics_state(
-				character_resource.character_state.physics_state(), latest_input_state)
-		
-		var current_health := character_resource.character_state.health_state().health()
-		for hitscan_result: HitscanResult in hitscan_results:
-			if hitscan_result.hit_entity_id == character_entity_id:
-				current_health -= hitscan_result.damage
-		var next_health_state := CharacterHealthState.new(current_health)
-		
-		var character_transform_state := CharacterTransformState.new(
-			next_physics_state.position(), latest_input_state.pitch(), latest_input_state.yaw())
-		character_components.first_person_display().display_character_state(character_transform_state, current_health)
-		character_components.third_person_display().display_character_transform(character_transform_state)
-		
-		next_world_state[character_entity_id] = ServerCharacterState.new(
-			next_physics_state, next_health_state)
-		data_to_export_per_client[client_id] = PerClientExportedData.new(
-			character_resource.client_tick,
-			next_physics_state,
-			next_health_state,
-			character_transform_state,
-			character_entity_id)
-		
+	var next_world_state := __compute_next_state_for_characters(character_resource_per_client_id, hitscan_results)
+	var data_to_export_per_client := __compile_data_to_export_per_client(
+		character_resource_per_client_id, next_world_state)
 	
-	__draw_bullet_tracers(hitscan_results)
+	__display_character_states(character_resource_per_client_id, next_world_state)
+	__draw_bullet_tracers(tracer_displayer, hitscan_results)
 
-	world_state_ = next_world_state
 	entity_spawner_.despawn_entities_not_in_server_snapshot(next_world_state)
 	__export_state_snapshots_to_clients(data_to_export_per_client)
+	world_state_ = next_world_state
 
 func __initialize_resources_for_new_client(client_id: int) -> void:
 	var client_character_entity: CharacterEntity = entity_creator_.create_character_entity()
@@ -124,11 +85,92 @@ func __export_state_snapshots_to_clients(data_to_export_per_client: Dictionary) 
 		messenger.send_message_to_client(client_id, state_snapshot_for_client.to_dict())
 	return state_snapshots_for_clients
 
-func __draw_bullet_tracers(hitscan_results: Array[HitscanResult]) -> void:
-	var tracer_displayer := entity_spawner_.get_or_create_tracer_displayer()
-	for hitscan_result: HitscanResult in hitscan_results:
-		tracer_displayer.add_tracer(hitscan_result.origin, hitscan_result.hit_point)
-	tracer_displayer.display_and_update_tracers()
+static func __prepare_character_resource_per_client_id(
+	client_resources_per_peer_id: Dictionary,
+	server_character_state_per_entity_id: Dictionary
+) -> Dictionary:
+	var character_resource_per_client_id: Dictionary = {}
+	for client_id: int in client_resources_per_peer_id:
+		var client_input_buffer_and_character: InputBufferAndCharacterEntity = client_resources_per_peer_id[client_id]
+		var latest_client_input_with_tick: QueueItem = client_input_buffer_and_character.input_buffer.pop()
+		var latest_client_input: ClientInput = latest_client_input_with_tick.value()
+		var client_tick_for_input: int = latest_client_input_with_tick.tick()
+		var character_entity_id: int = client_input_buffer_and_character.character_entity.entity_id
+		var character_state: ServerCharacterState = Utils.get_or_default(
+			server_character_state_per_entity_id, 
+			character_entity_id, 
+			client_input_buffer_and_character.character_entity.state_data)
+		character_resource_per_client_id[client_id] = ServerCharacterResource.new(
+			latest_client_input,
+			client_tick_for_input,
+			character_entity_id,
+			character_state,
+			client_input_buffer_and_character.character_entity.components)
+	return character_resource_per_client_id
+
+static func __compute_next_state_for_characters(
+	character_resource_per_client_id: Dictionary,
+	hitscan_results: Array[HitscanResult]
+) -> Dictionary:
+	var next_world_state := {}
+	for client_id: int in character_resource_per_client_id:
+		var character_resource: ServerCharacterResource = character_resource_per_client_id[client_id]
+		var character_entity_id := character_resource.character_entity_id
+		var character_components := character_resource.character_components
+		var latest_input_state := character_resource.input.input_state()
+		var next_physics_state := character_components.movement_body().compute_next_physics_state(
+				character_resource.character_state.physics_state(), latest_input_state)
+		
+		var current_health := character_resource.character_state.health_state().health()
+		for hitscan_result: HitscanResult in hitscan_results:
+			if hitscan_result.hit_entity_id == character_entity_id:
+				current_health -= hitscan_result.damage
+		var next_health_state := CharacterHealthState.new(current_health)
+		
+		next_world_state[character_entity_id] = ServerCharacterState.new(
+			next_physics_state, next_health_state)
+	return next_world_state
+
+static func __display_character_states(
+	character_resource_per_client_id: Dictionary,
+	next_character_state_per_entity_id: Dictionary
+) -> void:
+	for client_id: int in character_resource_per_client_id:
+		var character_resource: ServerCharacterResource = character_resource_per_client_id[client_id]
+		var character_entity_id := character_resource.character_entity_id
+		var next_character_state: ServerCharacterState = next_character_state_per_entity_id[character_entity_id]
+		var character_components := character_resource.character_components
+		var character_transform_state := __extract_transform_state(character_resource, next_character_state)
+		character_components.third_person_display().display_character_transform(character_transform_state)
+		character_components.first_person_display().display_character_state(
+			character_transform_state, next_character_state.health_state().health())
+
+static func __compile_data_to_export_per_client(
+	character_resource_per_client_id: Dictionary,
+	next_character_state_per_entity_id: Dictionary
+) -> Dictionary:
+	var data_to_export_per_client := {}
+	for client_id: int in character_resource_per_client_id:
+		var character_resource: ServerCharacterResource = character_resource_per_client_id[client_id]
+		var character_entity_id := character_resource.character_entity_id
+		var next_character_state: ServerCharacterState = next_character_state_per_entity_id[character_entity_id]
+		var next_transform_state := __extract_transform_state(character_resource, next_character_state)
+		data_to_export_per_client[client_id] = PerClientExportedData.new(
+			character_resource.client_tick_for_input,
+			next_character_state.physics_state(),
+			next_character_state.health_state(),
+			next_transform_state,
+			character_entity_id)
+	return data_to_export_per_client
+
+static func __extract_transform_state(
+	character_resource: ServerCharacterResource, 
+	character_state: ServerCharacterState
+) -> CharacterTransformState:
+	return CharacterTransformState.new(
+			character_state.physics_state().position(), 
+			character_resource.input.input_state().pitch(), 
+			character_resource.input.input_state().yaw())
 
 static func __perform_character_abilities(
 		world_state: Dictionary, 
@@ -153,6 +195,11 @@ static func __perform_character_abilities(
 			hitscan_ability_results.append_array(ability_result.hitscan_results)
 			trigger_ability_for_remote_character.rpc(character_entity_id)
 	return hitscan_ability_results
+
+static func __draw_bullet_tracers(tracer_displayer: TracerDisplayer, hitscan_results: Array[HitscanResult]) -> void:
+	for hitscan_result: HitscanResult in hitscan_results:
+		tracer_displayer.add_tracer(hitscan_result.origin, hitscan_result.hit_point)
+	tracer_displayer.display_and_update_tracers()
 
 static func __extract_states_for_remote_characters(
 	data_to_export_per_client: Dictionary, 
@@ -195,19 +242,19 @@ class InputBufferAndCharacterEntity:
 
 class ServerCharacterResource:
 	var input: ClientInput
-	var client_tick: int
+	var client_tick_for_input: int
 	var character_entity_id: int
 	var character_state: ServerCharacterState
 	var character_components: CharacterComponents
 
 	func _init(
 		input: ClientInput, 
-		client_tick: int,
+		client_tick_for_input: int,
 		character_entity_id: int,
 		current_physics_state: ServerCharacterState,
 		character_components: CharacterComponents) -> void:
 		self.input = input
-		self.client_tick = client_tick
+		self.client_tick_for_input = client_tick_for_input
 		self.character_entity_id = character_entity_id
 		self.character_state = current_physics_state
 		self.character_components = character_components
