e6726c3f8783e9b4a1d471e53fd70774b1809e30
PL3B3 <kevinqiesq@gmail.com>
2024-06-22 11:22:48 -0500

    Optionally use unreliable rpc's
    To test the robustness of the game to bad network conditions, let's
    allow using unreliable rpcs for the 2 main info streams:
    - client sending input to server
    - server sending state snapshots back to client
    
    We'll use clumsy for testing bad network over loopback
    https://jagt.github.io/clumsy/index.html
    For now, we focus on:
    - High latency
    - Packet loss
    - Packet jitter
    
    As these are the most likely issues under real network conditions.

--- game/network_messenger.gd
+++ game/network_messenger.gd
@@ -2,6 +2,7 @@ extends Node
 
 class_name NetworkMessenger
 
+const USE_RELIABLE_MESSAGING := false
 const SERVER_NETWORK_ID = 1
 const SEND_INTERVAL_USEC = "send_interval_USEC"
 const RECV_INTERVAL_USEC = "recv_interval_USEC"
@@ -13,18 +14,33 @@ func _ready():
 	LogsAndMetrics.add_client_stat(SEND_INTERVAL_USEC, 1000, true) 
 	LogsAndMetrics.add_server_stat(RECV_INTERVAL_USEC, 1000, true)
 
-@rpc("any_peer", "call_remote", "reliable")
+@rpc("any_peer", "call_remote", "unreliable")
 func _handle_message_from_client(message):
 	LogsAndMetrics.add_sample(RECV_INTERVAL_USEC, Time.get_ticks_usec())
 	emit_signal("received_client_message", multiplayer.get_remote_sender_id(), message)
 
-@rpc("authority", "call_remote", "reliable")
+@rpc("any_peer", "call_remote", "reliable")
+func _handle_message_from_client_reliable(message):
+	LogsAndMetrics.add_sample(RECV_INTERVAL_USEC, Time.get_ticks_usec())
+	emit_signal("received_client_message", multiplayer.get_remote_sender_id(), message)
+
+@rpc("authority", "call_remote", "unreliable")
 func _handle_message_from_server(message):
 	emit_signal("received_server_message", message)
 
+@rpc("authority", "call_remote", "reliable")
+func _handle_message_from_server_reliable(message):
+	emit_signal("received_server_message", message)
+
 func send_message_to_client(client_network_id, message):
-	_handle_message_from_server.rpc_id(client_network_id, message)
+	if USE_RELIABLE_MESSAGING:
+		_handle_message_from_server_reliable.rpc_id(client_network_id, message)	
+	else:
+		_handle_message_from_server.rpc_id(client_network_id, message)
 
 func send_message_to_server(message):
 	LogsAndMetrics.add_sample(SEND_INTERVAL_USEC, Time.get_ticks_usec())
-	_handle_message_from_client.rpc_id(SERVER_NETWORK_ID, message)
+	if USE_RELIABLE_MESSAGING:
+		_handle_message_from_client_reliable.rpc_id(SERVER_NETWORK_ID, message)
+	else:
+		_handle_message_from_client.rpc_id(SERVER_NETWORK_ID, message)
