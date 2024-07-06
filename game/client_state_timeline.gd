3d3365503ab946e45947cdd37614add8aa6428cd
PL3B3 <kevinqiesq@gmail.com>
2024-07-06 12:04:01 -0500

    Convert client state timeline to ringbuffer
    We don't need to hold onto every state since the game start: This causes
    memory usage to balloon. Instead, let's only retain the last 100 or so,
    which should be sufficient.
    
    In theory, there's a safety issue with retrieving items from the buffer
    because we could return something that doesn't have the same tick
    as what was requested (we only know the modulo is the same). This
    probably isn't a big danger now because these old states are only
    used for getting inputs for recon, and there is already a maximum
    ticks in the past limit on how old of inputs we can retrieve.

--- game/client_state_timeline.gd
+++ game/client_state_timeline.gd
@@ -1,29 +1,35 @@
-extends Object
+extends RefCounted
 class_name ClientStateTimeline
 
-var world_states_: Array[ClientStateSnapshot] = []
+const RING_BUFFER_SIZE := 100
 
-func add_next_state(next_state: ClientStateSnapshot):
-	world_states_.push_back(next_state)
-	return get_current_tick()
+var world_states_: Array[ClientStateSnapshot] = []
+var current_tick_: int = -1
+
+func _init() -> void:
+	world_states_.resize(RING_BUFFER_SIZE)
+
+func add_next_state(next_state: ClientStateSnapshot) -> int:
+	current_tick_ += 1
+	var index_in_ringbuffer_to_insert: int = current_tick_ % RING_BUFFER_SIZE
+	world_states_[index_in_ringbuffer_to_insert] = next_state
+	return current_tick_
 
 func get_state(tick: int) -> ClientStateSnapshot:
-	if tick < 0 or tick >= world_states_.size():
-		print("Cannot retrieve state for tick %d. Current tick is %d." % [tick, get_current_tick()])
-		return null
-	return world_states_[tick]
+	assert(tick > -1 and tick <= current_tick_, "Tick must be between 0 and timeline size %d" % current_tick_)
+	return world_states_[tick % RING_BUFFER_SIZE]
 
 func get_current_state() -> ClientStateSnapshot:
 	return get_state(get_current_tick())
 
-func get_current_tick():
-	return world_states_.size() - 1
+func get_current_tick() -> int:
+	return current_tick_
 
-func get_next_tick():
+func get_next_tick() -> int:
 	return get_current_tick() + 1
 
-func has_states():
-	return world_states_.size() > 0
+func has_states() -> bool:
+	return current_tick_ > -1
 
 func get_inputs_since_tick(start_tick_inclusive: int) -> Array[InputState]:
 	var inputs_since_start_tick_inclusive: Array[InputState] = []
