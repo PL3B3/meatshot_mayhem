ccb7300dbfdd91f42fa04fbe67dfe7da021554b9
PL3B3 <kevinqiesq@gmail.com>
2024-07-14 19:23:33 -0500

    Make server state dataclass an inner class
    Nothing else needs to know about this dataclass. It's simpler to have
    all the server state datamodels live in one place. We can move them
    out as a group once we've factored out the logic that touches state into
    its own unit.

--- game/server.gd
+++ game/server.gd
@@ -418,3 +418,17 @@ class CharacterEntity extends Entity:
 		super(entity_id)
 		self.state_data = state_data
 		self.components = components
+
+class ServerCharacterState:
+	var physics_state: CharacterPhysicsState
+	var health_state: CharacterHealthState
+
+	func _init(physics_state: CharacterPhysicsState, health_state: CharacterHealthState) -> void:
+		self.physics_state = physics_state
+		self.health_state = health_state
+
+	func _to_string() -> String:
+		return "ServerCharacterState<physics_state=%s, health_state=%s>" % [
+			physics_state,
+			health_state    
+		]
