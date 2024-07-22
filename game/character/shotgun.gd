53f9202e1cfab2e442066a6f34792641aeaeb209
PL3B3 <kevinqiesq@gmail.com>
2024-07-22 18:54:31 -0500

    Update shotgun model to single-barrel
    With a double-barreled gun, tracers coming from a single origin looks
    strange. So we'd have to find a way to specify origins for each bullet.
    It's simpler to just change the gun model vs. figuring out that system
    right now.

--- game/character/shotgun.gd
+++ game/character/shotgun.gd
@@ -1,6 +1,7 @@
 class_name Shotgun extends Node3D
 
 @onready var shoot_animation_player_: AnimationPlayer = $ShootAnimationPlayer
+@onready var gun_model_: MeshInstance3D = $GunModel
 
 func play_first_person_shoot_animation() -> void:
 	shoot_animation_player_.stop()
@@ -9,3 +10,6 @@ func play_first_person_shoot_animation() -> void:
 func play_third_person_shoot_animation() -> void:
 	shoot_animation_player_.stop()
 	shoot_animation_player_.play("shoot_third_person")
+
+func get_tracer_origin_position() -> Vector3:
+	return gun_model_.global_position
