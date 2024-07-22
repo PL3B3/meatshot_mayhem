4d2339255f83f2d202e3cd9d49555733deef1226
PL3B3 <kevinqiesq@gmail.com>
2024-07-22 12:00:24 -0500

    Extract gun model and animation to separate scene
    It's simpler for the character display class to just tell the shotgun to
    handle its own animation, rather than trying to use the animation tree.
    
    The one downside of this is we won't get the gun model updated
    automatically we export a new model from blender, but this is just
    because export the gun and character together into the same Godot
    scene. Not something worth addressing right now.

---
+++ game/character/shotgun.gd
@@ -0,0 +1,11 @@
+class_name Shotgun extends Node3D
+
+@onready var shoot_animation_player_: AnimationPlayer = $ShootAnimationPlayer
+
+func play_first_person_shoot_animation() -> void:
+	shoot_animation_player_.stop()
+	shoot_animation_player_.play("shoot_first_person")
+
+func play_third_person_shoot_animation() -> void:
+	shoot_animation_player_.stop()
+	shoot_animation_player_.play("shoot_third_person")
