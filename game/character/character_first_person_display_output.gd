81db126f6c18dce93d654fbe4efece5d2464a2d1
PL3B3 <kevinqiesq@gmail.com>
2024-07-22 12:00:24 -0500

    Extract gun model and animation to separate scene
    It's simpler for the character display class to just tell the shotgun to
    handle its own animation, rather than trying to use the animation tree.
    
    The one downside of this is we won't get the gun model updated
    automatically we export a new model from blender, but this is just
    because export the gun and character together into the same Godot
    scene. Not something worth addressing right now.

--- game/character/character_first_person_display_output.gd
+++ game/character/character_first_person_display_output.gd
@@ -6,7 +6,7 @@ const CAMERA_OFFSET_FROM_CHARACTER_ORIGIN = Vector3(0, 0.75, 0)
 
 @onready var camera_: Camera3D = $Camera3D
 @onready var health_label_: Label = $HealthLabel
-@onready var gun_animation_player_: AnimationPlayer = $GunRecoilAnimationPlayer
+@onready var shotgun_: Shotgun = $Camera3D/Shotgun
 
 func display_character_state(character_transform: CharacterTransformState, health: int) -> void:
 	camera_.transform = compute_camera_transform(character_transform)
@@ -20,5 +20,4 @@ func compute_camera_transform(character_transform: CharacterTransformState) -> T
 	return Transform3D(mouse_look_rotation_basis, camera_position)
 
 func play_fire_gun_animation() -> void:
-	gun_animation_player_.stop()
-	gun_animation_player_.play("gun_recoil")
+	shotgun_.play_first_person_shoot_animation()
