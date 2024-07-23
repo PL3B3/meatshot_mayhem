061d3710375fa5495f911dff4a8016fe6a022e09
PL3B3 <kevinqiesq@gmail.com>
2024-07-22 19:01:07 -0500

    Slightly reduce fire rate
    Twice a second feels too frequent.
    Spurious scene change included (the editor keeps modifying the scene)

--- game/character/character_ability_trigger_state_machine.gd
+++ game/character/character_ability_trigger_state_machine.gd
@@ -1,7 +1,7 @@
 extends RefCounted
 class_name CharacterAbilityTriggerStateMachine
 
-const MINIMUM_TICK_INTERVAL_BETWEEN_TRIGGERS: int = 30
+const MINIMUM_TICK_INTERVAL_BETWEEN_TRIGGERS: int = 40
 const TRIGGERED := true
 const NOT_TRIGGERED := false
 
