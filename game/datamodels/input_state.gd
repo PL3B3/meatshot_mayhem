cc32669ca7e236ef3bed2e1aee673a1d570bf44f
PL3B3 <kevinqiesq@gmail.com>
2024-07-23 21:40:22 -0500

    Add client tick to InputState
    It's easier to have the tick be part of the InputState datamodel because
    they are always accessed together and almost always set together. The
    one exception might be for extrapolated inputs on the server side, aka
    when the server input buffer repeats the last received input in the case
    where the buffer is empty from packet losses.

--- game/datamodels/input_state.gd
+++ game/datamodels/input_state.gd
@@ -2,22 +2,31 @@ extends RefCounted
 
 class_name InputState
 
-enum INPUT_STATE_KEY { YAW, PITCH, IS_JUMPING, IS_SLOW_WALKING, DIRECTION }
+enum INPUT_STATE_KEY { YAW, PITCH, IS_JUMPING, IS_SLOW_WALKING, DIRECTION, CLIENT_TICK }
 
-static var DEFAULT = InputState.new(0, 0, false, false, Vector2())
+static var DEFAULT := InputState.new(0, 0, false, false, Vector2(), 0)
 
 var yaw_: float
 var pitch_: float
 var is_jumping_: bool
 var is_slow_walking_: bool
 var direction_: Vector2
+var client_tick_: int
 
-func _init(yaw, pitch, is_jumping, is_slow_walking, direction):
+func _init(
+	yaw: float, 
+	pitch: float, 
+	is_jumping: bool, 
+	is_slow_walking: bool, 
+	direction: Vector2, 
+	client_tick: int
+) -> void:
 	yaw_ = yaw
 	pitch_ = pitch
 	is_jumping_ = is_jumping
 	is_slow_walking_ = is_slow_walking
 	direction_ = direction
+	client_tick_ = client_tick
 
 func yaw():
 	return yaw_
@@ -34,22 +43,27 @@ func is_slow_walking():
 func direction():
 	return direction_
 
+func client_tick():
+	return client_tick_
+
 func to_dict() -> Dictionary:
 	return {
 		INPUT_STATE_KEY.YAW: yaw_,
 		INPUT_STATE_KEY.PITCH: pitch_,
 		INPUT_STATE_KEY.IS_JUMPING: is_jumping_,
 		INPUT_STATE_KEY.IS_SLOW_WALKING: is_slow_walking_,
-		INPUT_STATE_KEY.DIRECTION: direction_
+		INPUT_STATE_KEY.DIRECTION: direction_,
+		INPUT_STATE_KEY.CLIENT_TICK: client_tick_
 	}
 
-static func from_dict(serialized_data: Dictionary):
+static func from_dict(serialized_data: Dictionary) -> InputState:
 	return InputState.new(
 		serialized_data[INPUT_STATE_KEY.YAW],
 		serialized_data[INPUT_STATE_KEY.PITCH],
 		serialized_data[INPUT_STATE_KEY.IS_JUMPING],
 		serialized_data[INPUT_STATE_KEY.IS_SLOW_WALKING],
-		serialized_data[INPUT_STATE_KEY.DIRECTION]
+		serialized_data[INPUT_STATE_KEY.DIRECTION],
+		serialized_data[INPUT_STATE_KEY.CLIENT_TICK]
 	)
 
 func _to_string():
