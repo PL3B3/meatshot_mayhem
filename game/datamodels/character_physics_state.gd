d20f6fc5c0a0626326e850944e82c03e8afa107e
PL3B3 <kevinqiesq@gmail.com>
2024-07-25 21:45:41 -0500

    Tighten range for own position y axis serde
    We were seeing slight up and down jittering after enabling byte
    serde for state snapshots. This seemed to be an artefact with client
    side reconciliation. Specifically, when resting on the floor, the
    deserialized own-character-position we receive from the server would
    be slightly different from the predicted position because we map the
    y position to a 16-bit int, so it's not as granular as a double.
    
    To fix, let's restrict the domain of the y position for serialization,
    so that there is more granularity in the deserialized value. In the long
    term, we want to separate own character state snapshots from the
    state for the rest of the world, which will make it easier to send a
    highly precise state without sacrificing bandwidth, because we can send
    at a different cadence.

--- game/datamodels/character_physics_state.gd
+++ game/datamodels/character_physics_state.gd
@@ -41,7 +41,7 @@ static func from_dict(dict: Dictionary) -> CharacterPhysicsState:
 
 func serialize_to_stream(serialized_data_stream: StreamPeerBuffer) -> void:
 	serialized_data_stream.put_u16(SerdeUtil.POSITION_AXIS_SERDE.serialize_float_to_u16(position_.x))
-	serialized_data_stream.put_u16(SerdeUtil.POSITION_AXIS_SERDE.serialize_float_to_u16(position_.y))
+	serialized_data_stream.put_u16(SerdeUtil.POSITION_Y_AXIS_SERDE.serialize_float_to_u16(position_.y))
 	serialized_data_stream.put_u16(SerdeUtil.POSITION_AXIS_SERDE.serialize_float_to_u16(position_.z))
 	serialized_data_stream.put_u16(SerdeUtil.VELOCITY_AXIS_SERDE.serialize_float_to_u16(velocity_.x))
 	serialized_data_stream.put_u16(SerdeUtil.VELOCITY_AXIS_SERDE.serialize_float_to_u16(velocity_.y))
@@ -52,7 +52,7 @@ static func consume_and_deserialize(serialized_data_stream: StreamPeerBuffer) ->
 	return CharacterPhysicsState.new(
 		Vector3(
 			SerdeUtil.POSITION_AXIS_SERDE.deserialize_float_from_u16(serialized_data_stream.get_u16()),
-			SerdeUtil.POSITION_AXIS_SERDE.deserialize_float_from_u16(serialized_data_stream.get_u16()),
+			SerdeUtil.POSITION_Y_AXIS_SERDE.deserialize_float_from_u16(serialized_data_stream.get_u16()),
 			SerdeUtil.POSITION_AXIS_SERDE.deserialize_float_from_u16(serialized_data_stream.get_u16())),
 		Vector3(
 			SerdeUtil.VELOCITY_AXIS_SERDE.deserialize_float_from_u16(serialized_data_stream.get_u16()),
