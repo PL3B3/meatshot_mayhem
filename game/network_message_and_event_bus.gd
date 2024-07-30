20fd4b40f2b00521eda806e3f7235203b14e0fe6
PL3B3 <kevinqiesq@gmail.com>
2024-07-29 21:13:30 -0500

    Add stats for rpc receive interval
    We want to measure the interval between:
    - Client inputs received on server
    - State snapshots received by client
    
    This captures network jitter + jitter in the send interval. The latter
    is relevant because we call RPCs during the physics tick. This can
    be problematic because the physics tick does not actually run at a
    fixed interval. Rather, the engine has a semi-fixed time step. This
    means that the physics steps actually run as a loop within the render
    step, and the number of physics steps roughly depends on how long the
    frame took. So for any given frame of time (i.e. around 16ms IRL) there
    could be 0, 1, 2, or more physics frames. Typically, the RPC receiver
    experiences this as "2 packets arrive in quick succession" or
    "there is a long gap between 2 packets arriving."
    
    Let's measure this so we can see how bad it is and know when we've
    fixed it.

--- game/network_message_and_event_bus.gd
+++ game/network_message_and_event_bus.gd
@@ -15,11 +15,13 @@ signal server_disconnected()
 signal respawned()
 signal died()
 
+var __input_receive_interval_usec_stat_per_client_id := {}
+var __state_snapshot_receive_interval_usec_stat: Statistics
 
 func _ready() -> void:
 	name = "NetworkBus"
-	multiplayer.peer_connected.connect(__on_client_connected)
-	multiplayer.peer_disconnected.connect(func(client_id: int) -> void: client_disconnected.emit(client_id))
+	multiplayer.peer_connected.connect(__register_new_client)
+	multiplayer.peer_disconnected.connect(__deregister_client)
 	multiplayer.server_disconnected.connect(func() -> void: server_disconnected.emit())
 
 func notify_client_of_death(client_id: int) -> void:
@@ -58,9 +60,13 @@ func quit_to_client_menu() -> void:
 # Name is shortened to make the RPC packet size smaller
 @rpc("any_peer", "call_remote", "unreliable")
 func c2s(serialized_inputs: Array) -> void:
+	var source_client_id: int = multiplayer.get_remote_sender_id()
+	if source_client_id in __input_receive_interval_usec_stat_per_client_id:
+		var input_receive_interval_stat: Statistics = __input_receive_interval_usec_stat_per_client_id[source_client_id]
+		input_receive_interval_stat.add_sample(Time.get_ticks_usec())
 	for serialized_input: PackedByteArray in serialized_inputs:
 		var client_input := InputState.deserialize(serialized_input)
-		received_client_input.emit(multiplayer.get_remote_sender_id(), client_input)
+		received_client_input.emit(source_client_id, client_input)
 
 # Name is shortened to make the RPC packet size smaller
 @rpc("any_peer", "call_remote", "reliable")
@@ -70,6 +76,11 @@ func ct2s(camera_transform: Transform3D, server_tick_displayed: int) -> void:
 # Name is shortened to make the RPC packet size smaller
 @rpc("authority", "call_remote", "unreliable")
 func s2c(state_snapshot_server_tick: int, client_tick: int, serialized_state_snapshot: PackedByteArray) -> void:
+	if __state_snapshot_receive_interval_usec_stat == null:
+		var own_client_id: int = multiplayer.get_unique_id()
+		__state_snapshot_receive_interval_usec_stat = LogsAndMetrics.add_universal_stat(
+			"client_state_snapshot_receive_interval_usec[client=%10d]" % own_client_id, 10, true)
+	__state_snapshot_receive_interval_usec_stat.add_sample(Time.get_ticks_usec())
 	var deserialized_state_snapshot := ClientStateSnapshot.deserialize(serialized_state_snapshot)
 	received_authoritative_state_snapshot.emit(state_snapshot_server_tick, client_tick, deserialized_state_snapshot)
 
@@ -96,12 +107,18 @@ func __resize_window_for_debugging(index: int = 0) -> void:
 		get_window().size = Vector2(screen_size.x / 2.01, screen_size.y / 2)
 		get_window().position = Vector2(screen_size.x + (index * (screen_size.x * 0.5)), 0)
 
-func __on_client_connected(client_id: int) -> void:
+func __register_new_client(client_id: int) -> void:
 	if multiplayer.is_server():
 		var prior_peer_count: int = multiplayer.get_peers().size() - 1
 		__resize_window_for_debugging.rpc_id(client_id, prior_peer_count)
 		__resize_server_window_for_debugging(prior_peer_count)
 		client_connected.emit(client_id)
+		__input_receive_interval_usec_stat_per_client_id[client_id] = LogsAndMetrics.add_universal_stat(
+			"server_input_receive_interval_usec[client=%10d]" % client_id, 10, true)
+
+func __deregister_client(client_id: int) -> void:
+	__input_receive_interval_usec_stat_per_client_id.erase(client_id)
+	client_disconnected.emit(client_id)
 
 func __resize_server_window_for_debugging(index: int = 0)  -> void:
 	if OS.is_debug_build():
