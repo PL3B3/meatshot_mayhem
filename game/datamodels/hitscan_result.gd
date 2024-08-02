1a9cfe318e2b7fbd289277a6751cab7b38e8595b
PL3B3 <kevinqiesq@gmail.com>
2024-08-01 21:08:51 -0500

    Move all server simulation data to SimulationState
    Currently there are two pieces of data which live outside the central
    SimulationState dataclass:
    - Hitscan ability results
    - The moved character states returned by the physics computation
    
    These pieces of data are separately passed into downstream functions,
    independent of the SimulationState. This has the benefit of making
    those functions' dependencies quite clear, but the major downside that
    there isn't one clear view what the state is at any given time.
    
    This change has 2 main parts:
    - Move hitscan results into the simulation state
    - Change the functions that contain the main game logic from static to
      instance, having them directly reference and mutate the sim state

--- game/datamodels/hitscan_result.gd
+++ game/datamodels/hitscan_result.gd
@@ -12,6 +12,9 @@ func _init(origin: Vector3, hit_point: Vector3, hit_entity_id: int, damage: int)
 	self.hit_entity_id = hit_entity_id
 	self.damage = damage
 
+func duplicate() -> HitscanResult:
+	return HitscanResult.new(origin, hit_point, hit_entity_id, damage)
+
 func with_origin(new_origin: Vector3) -> HitscanResult:
 	return HitscanResult.new(new_origin, hit_point, hit_entity_id, damage)
 
--- version.gd
+++ version.gd
@@ -1,2 +1,2 @@
 class_name Version
-const VERSION := "2024-08-01-20:43:32::jIfjvRMQ5Rnjq+B5"
+const VERSION := "2024-08-01-21:17:54::uB9wtdX4NqpdVYf4"
